adc34f30e0dc44b50e441696575a51f9a45999b8
Subhajit Sahu <wolfram77@gmail.com>
2025-12-28 11:00:15 +0530

    :bug: with examples

--- build.sh
+++ build.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
-URL="https://raw.githubusercontent.com/phoboslab/pl_mpeg/refs/heads/master/pl_mpeg.h"
+# Fetch the latest version of the library
+fetch() {
+if [ -d "pl_mpeg" ]; then return; fi
+URL="https://github.com/phoboslab/pl_mpeg/raw/refs/heads/master/pl_mpeg.h"
 FILE="pl_mpeg.h"
 
 # Download the release
-if [ ! -f "$FILE" ]; then
-  echo "Downloading $FILE from $URL ..."
-  curl -L "$URL" -o "$FILE"
-  echo ""
-fi
+mkdir -p "pl_mpeg"
+echo "Downloading $FILE from $URL ..."
+curl -L "$URL" -o "pl_mpeg/$FILE"
+echo ""
+}
+
+
+# Test the project
+test() {
+echo "Running 01-extract-frames.c ..."
+clang -I. -o 01.exe examples/01-extract-frames.c
+./01.exe examples/earth-480p.mpg && echo -e "\n"
+}
+
+
+# Main script
+if [[ "$1" == "test" ]]; then test
+elif [[ "$1" == "fetch" ]]; then fetch
+else echo "Usage: $0 {fetch|test}"; fi
---
+++ pl_mpeg.h
@@ -0,0 +1,6 @@
+#pragma once
+#ifndef PLM_NO_STDIO
+#include <stdio.h>
+#endif
+
+#include "pl_mpeg/pl_mpeg.h"
